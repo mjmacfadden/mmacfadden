aa65c68de2d810cb30c9203402a8f615fd5a30fa
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2024-10-02 10:59:44 -0500

    foo

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
     <meta name="twitter:description" content="<?php echo htmlspecialchars($og_description); ?>">
     <meta name="twitter:image" content="<?php echo htmlspecialchars($og_image); ?>">
 
-    <base href="/">
+    <base href="/public/">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/style.css">
