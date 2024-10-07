9b05c53d470087ef4d698f20d5d7fabcae96054c
Mike Macfadden <mikemacfadden@gmail.com>
2024-10-06 21:22:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,35 @@
 // Select all elements with the class 'hoverImage'
-const images = document.querySelectorAll('.hoverImage');
+const images = document.querySelectorAll(".hoverImage");
 
 // Loop through each image and add event listeners for hover
 images.forEach((img) => {
-    // Change the extension to .gif on hover
-    img.addEventListener('mouseenter', function() {
-        this.src = this.src.replace('.jpg', '.gif');
-    });
+  // Change the extension to .gif on hover
+  img.addEventListener("mouseenter", function () {
+    this.src = this.src.replace(".jpg", ".gif");
+  });
 
-    // Change the extension back to .jpg when hover is over
-    img.addEventListener('mouseleave', function() {
-        this.src = this.src.replace('.gif', '.jpg');
-    });
+  // Change the extension back to .jpg when hover is over
+  img.addEventListener("mouseleave", function () {
+    this.src = this.src.replace(".gif", ".jpg");
+  });
 });
 
 //Focus Input
-var myModal = document.getElementById('searchModal')
-var myInput = document.getElementById('searchInput')
+var myModal = document.getElementById("searchModal");
+var myInput = document.getElementById("searchInput");
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
-
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
 
 // Create an object with old paths as keys and new paths as values
 const redirects = {
-    "/mark-more.html": "/markmore.php",
-    "/templates/": "/templates.html",
-    "/templates": "/templates.html",
-    "/mark-more": "/markmore.html"
+  "/mark-more.html": "/markmore.php",
+  "/templates/": "/templates.html",
+  "/Templates/": "/templates.html",
+  "/templates": "/templates.html",
+  "/mark-more": "/markmore.html",
+  "/about/": "/about.html",
 };
 
 // Get the current path
@@ -36,6 +37,6 @@ const currentPath = window.location.pathname;
 
 // Check if the current path exists in the redirects object
 if (redirects[currentPath]) {
-    // Redirect to the new URL
-    window.location.href = redirects[currentPath];
+  // Redirect to the new URL
+  window.location.href = redirects[currentPath];
 }
