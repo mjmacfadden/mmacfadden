52f81068687f0aa210b16a21dd1927df50696f3a
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-02-14 09:44:32 -0600

    collage portfolio

---
+++ collage-portfolio.php
@@ -0,0 +1,85 @@
+<?php 
+  $page_title = "Mike Macfadden - Collage Portfolio";
+  $meta_description = "Explore the unique collage art of Mike Macfadden, blending digital and traditional techniques to create visually striking compositions.";
+  $og_title = "Mike Macfadden - Collage Portfolio";
+  //$og_url = "https://yourwebsite.com/custom-page";
+  //$og_image = "https://yourwebsite.com/custom-image.jpg";
+
+  include 'header.php';
+?>
+
+<div class="page_header">
+  <div class="container py-4">
+    <div class="row py-4">
+      <!-- First Row -->
+      <div class="col">
+        <h1>Collage</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container py-4 collage-portfolio">
+  <div class="row py-4">
+    <div class="col-lg-2 col-md-4 p-3">
+        <img class="img-fluid" src="img/collage/0018.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0017.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+        <img class="img-fluid" src="img/collage/0016.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0015.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0014.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0013.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+        <img class="img-fluid" src="img/collage/0012.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0011.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0010.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0009.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+        <img class="img-fluid" src="img/collage/0008.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0007.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0006.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0005.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+        <img class="img-fluid" src="img/collage/0004.jpg">
+    </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0003.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0002.jpg">
+      </div>
+    <div class="col-lg-2 col-md-4 p-3">
+          <img class="img-fluid" src="img/collage/0001.jpg">
+    </div>
+
+
+  </div>
+</div>
+
+<?php
+    include 'footer.php'; // Include footer
+?>
